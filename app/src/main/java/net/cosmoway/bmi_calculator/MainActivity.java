eeee021719c813753c0d2486b5b42739e6fb977b
Yuuki Shimizu <shimizu@cosmoway.net>
2016-07-20 20:17:04 +0900

    calc ボタンにリスナーを設定

--- app/src/main/java/net/cosmoway/bmi_calculator/MainActivity.java
+++ app/src/main/java/net/cosmoway/bmi_calculator/MainActivity.java
@@ -2,14 +2,15 @@ package net.cosmoway.bmi_calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mHeightEditText;
     private EditText mWeightEditText;
-    private Button mCalculateBmiButtom;
+    private Button mCalculateBmiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViews();
+        setListeners();
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
     private void findViews() {
         mHeightEditText = (EditText) findViewById(R.id.edittext_height);
         mWeightEditText = (EditText) findViewById(R.id.edittext_weight);
-        mCalculateBmiButtom = (Button) findViewById(R.id.button_calculate_bmi);
+        mCalculateBmiButton = (Button) findViewById(R.id.button_calculate_bmi);
+    }
+
+    private void setListeners() {
+        mCalculateBmiButton.setOnClickListener(this);
     }
 }
